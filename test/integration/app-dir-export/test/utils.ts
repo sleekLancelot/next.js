45444a4eb999d19318a8868586a9190ffe7d1f90
devjiwonchoi <devjiwonchoi@gmail.com>
2025-08-04 18:25:45 +0200

    test: update export snapshot

--- test/integration/app-dir-export/test/utils.ts
+++ test/integration/app-dir-export/test/utils.ts
@@ -37,6 +37,8 @@ export const expectedWhenTrailingSlashTrue = [
     ? [expect.stringMatching(/_next\/static\/media\/favicon\.[0-9a-f]+\.ico/)]
     : []),
   expect.stringMatching(/_next\/static\/media\/test\.[0-9a-f]+\.png/),
+  '_next/static/metadata/favicon.ico',
+  '_next/static/metadata/robots.txt',
   '_next/static/test-build-id/_buildManifest.js',
   ...(process.env.IS_TURBOPACK_TEST
     ? ['_next/static/test-build-id/_clientMiddlewareManifest.json']
@@ -52,12 +54,10 @@ export const expectedWhenTrailingSlashTrue = [
   'api/txt',
   'client/index.html',
   'client/index.txt',
-  'favicon.ico',
   'image-import/index.html',
   'image-import/index.txt',
   'index.html',
   'index.txt',
-  'robots.txt',
 ]
 
 const expectedWhenTrailingSlashFalse = [
