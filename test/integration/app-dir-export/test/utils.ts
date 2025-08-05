5e662031daa7c5e4f6a0bf806df6c12c316a83b1
devjiwonchoi <devjiwonchoi@gmail.com>
2025-08-05 22:57:46 +0200

    test: update export test snapshot

--- test/integration/app-dir-export/test/utils.ts
+++ test/integration/app-dir-export/test/utils.ts
@@ -67,6 +67,8 @@ const expectedWhenTrailingSlashFalse = [
     ? [expect.stringMatching(/_next\/static\/media\/favicon\.[0-9a-f]+\.ico/)]
     : []),
   expect.stringMatching(/_next\/static\/media\/test\.[0-9a-f]+\.png/),
+  '_next/static/metadata/favicon.ico',
+  '_next/static/metadata/robots.txt',
   '_next/static/test-build-id/_buildManifest.js',
   ...(process.env.IS_TURBOPACK_TEST
     ? ['_next/static/test-build-id/_clientMiddlewareManifest.json']
@@ -82,12 +84,10 @@ const expectedWhenTrailingSlashFalse = [
   'api/txt',
   'client.html',
   'client.txt',
-  'favicon.ico',
   'image-import.html',
   'image-import.txt',
   'index.html',
   'index.txt',
-  'robots.txt',
 ]
 
 export async function getFiles(cwd = exportDir) {
